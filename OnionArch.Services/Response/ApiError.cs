8b901a503320edc34dffcc486bf8e43b4d96b885
mho <mohamed.hosny@intalio.com>
2023-10-12 10:14:05 +0300

    generic response

---
+++ OnionArch.Services/Response/ApiError.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArch.Services.Response
+{
+    public class ApiError
+    {
+        public string ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+        public string ErrorDetails { get; set; }
+    }
+}
---
+++ OnionArch.Services/Response/ApiResponse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArch.Services.Response
+{
+    public class ApiResponse<T>
+    {
+        public ApiResponse()
+        {
+        }
+
+        // Success Response
+        public ApiResponse(T data)
+        {
+            Succeeded = true;
+            Data = data;
+        }
+
+        // Failed Response
+        public ApiResponse(string errorMessage)
+        {
+            Succeeded = false;
+            Message = errorMessage;
+        }
+
+        public bool Succeeded { get; set; }
+        public string? Message { get; set; }
+        public List<ApiError>? Errors { get; set; }
+        public T? Data { get; set; }
+        public int? StatusCode { get; set; }
+        public double? RequestDurationMilliseconds { get; set; }
+        public string? ApiVersion { get; set; }
+    }
+}
